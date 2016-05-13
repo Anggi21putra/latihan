d1a188b4e87091d84a15324179c9f1a2a32afb1a
Anggi <putraanggi25@gmail.com>
2016-05-13 23:40:00 +0700

    ada perubahan pada struktur if else

--- if1.java
+++ if1.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 
 Byte nilai = 90;
 if(nilai > 80)
-System.out.println("Selamat Anda Lulus");
+System.out.println("Selamat Anda Lulus, dengan Cum Lauuuddeeeeee.... di biyayain lansung S2 ke Togo...");
 System.out.println("dengan Nilai: "+nilai);
 }
 }
